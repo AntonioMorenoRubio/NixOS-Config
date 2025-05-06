4b5a5000a3d7048ebe81848cbc1a57a75ebd4b97
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-05-06 20:58:07 +0200

    Added: git to packages.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -11,6 +11,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     ntfs3g
+    git
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
