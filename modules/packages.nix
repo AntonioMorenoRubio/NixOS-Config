498642fc5f42efb2007b8fe99121aa92bf7a12f2
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-05-08 08:16:45 +0200

    Added: Discord, Telegram, WhatsApp (ZapZap)

--- modules/packages.nix
+++ modules/packages.nix
@@ -38,6 +38,11 @@ in
     protonplus
     lutris
 
+    # Comunicaciones
+    discord
+    telegram-desktop
+    zapzap
+
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
