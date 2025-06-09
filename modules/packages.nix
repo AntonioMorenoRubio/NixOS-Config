557e2825f2d27fcdd9d54ca06e196867a29ecbda
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-06-09 16:45:13 +0200

    Fixed white spaces for Steam configuration

--- modules/packages.nix
+++ modules/packages.nix
@@ -15,9 +15,9 @@ in
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
-    remotePlay.openFirewall = true; # Open ports in the   firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the   firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open   ports in the firewall for Steam Local Network Game   Transfers
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
   # Modo juego para el Sistema Operativo
