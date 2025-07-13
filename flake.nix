23f41a30879fd6cbf8e88ae115bb5729b1afda37
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-07-13 13:01:23 +0200

    Refactor: Autofirma output moved to modules/autofirma.nix

--- flake.nix
+++ flake.nix
@@ -49,37 +49,7 @@
             (import ./modules/aagl.nix { inherit aagl; })
 
             autofirma-nix.nixosModules.default
-
-            ({ config, pkgs, ... }: {
-              # The autofirma command becomes available system-wide
-              programs.autofirma = {
-                enable = true;
-                firefoxIntegration.enable = true;
-              };
-
-              # DNIeRemote integration for using phone as NFC reader
-              programs.dnieremote = {
-                enable = true;
-              };
-
-              # The FNMT certificate configurator
-              programs.configuradorfnmt = {
-                enable = true;
-                firefoxIntegration.enable = true;
-              };
-
-              # Firefox configured to work with AutoFirma
-              programs.firefox = {
-                enable = true;
-                policies.SecurityDevices = {
-                  "OpenSC PKCS#11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
-                  "DNIeRemote" = "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
-                };
-              };
-
-              # Enable PC/SC smart card service
-              services.pcscd.enable = true;
-            })
+            ./modules/autofirma.nix
           ];
         };
       };
