10b49af2c2dc2a7bd782b6579557031ef996e4ab
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-08-14 02:52:50 +0200

    Added: weekly auto-optimizing for NixOS store

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,5 +7,14 @@
             dates = "weekly";
             options = "--delete-older-than 14d";
         };
+
+        optimise = {
+            automatic = true;
+            dates = [ "weekly" ];
+        };
+
+        settings = {
+            auto-optimise-store = false; # If false, use 'nix-store --optimise' manually. WARNING: Can be long operation
+        };
     };
 }
