af06f1f1f87143b99b23f79cae8c1082804088b8
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-08-15 23:37:19 +0200

    Refactor: programs in my home-manager

--- home/antoniomr.nix
+++ home/antoniomr.nix
@@ -6,13 +6,17 @@
 
   home.stateVersion = "25.05"; # ¡NO lo cambies una vez definido!
 
-  programs.home-manager.enable = true;
-
   # Ejemplo de aplicaciones y configuración
-  programs.git = {
-    enable = true;
-    userName = "AntonioMorenoRubio";
-    userEmail = "amrinformatica10@gmail.com";
+  programs = {
+	home-manager = {
+	    enable = true;
+	};
+
+	git = {
+	    enable = true;
+	    userName = "AntonioMorenoRubio";
+	    userEmail = "amrinformatica10@gmail.com";
+	};
   };
 
   home.packages = with pkgs; [
